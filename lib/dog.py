3e8be763be9a8be9072e65c864245c8ee7243cde
+ <+githinjiadreen27@gmail.com>
2025-08-09 09:20:06 +0300

    Adding Dog class with Validation

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,25 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,name="Luffie",breed="Mastiff"): # Constructor method
+        # Name validation
+        self.name = name
+
+        if not isinstance(name, str) or len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name   
+
+             
+        self.breed = breed
+
+    @property
+    def breed(self):
+        return self._breed    
+
+    @breed.setter
+    def breed(self, value):
+        if value not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
