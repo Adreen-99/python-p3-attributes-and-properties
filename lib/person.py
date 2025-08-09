8f56dbf8295a110b6f29ff960f9a0dbc629dd592
+ <+githinjiadreen27@gmail.com>
2025-08-09 09:21:46 +0300

    Adding Person class with Validation

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,23 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name="Unknown",job="Engineer"): # Constructor method
+        self.name = name
+        if not isinstance(name, str) or len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name.title()
+
+        self.job = job
+
+    @property
+    def job(self):
+        return self._job        
+    
+    @job.setter
+    def job(self, value):
+        if value not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
+
